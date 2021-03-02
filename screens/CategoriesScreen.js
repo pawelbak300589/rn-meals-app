f251aca1bb696c6cacc7ad4a4694b58587503ca2
Pawel Bak <pawelbak300589@gmail.com>
2021-03-02 20:38:27 +0000

    Update Categories, CategoryMeals & MealDetail screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = (props) => {
+
+  const renderGridItem = (itemData) => {
+    return <CategoryGridTile
+      title={itemData.item.title}
+      color={itemData.item.color}
+      onSelect={() => {
+        props.navigation.navigate({
+          routeName: 'CategoryMeals',
+          params: {
+            categoryId: itemData.item.id
+          }
+        })
+      }} />;
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button title="Go to Meals!" onPress={() => {
-        props.navigation.navigate({ routeName: 'CategoryMeals' });
-      }} />
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories'
 };
 
 const styles = StyleSheet.create({
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+            params: {
+              mealId: itemData.item.id
+            }
+          })
+        }} />
+    );
+  };
+
+  const catId = props.navigation.getParam('categoryId');
+
+  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen!</Text>
-      <Button title="Go to Details" onPress={() => {
-        props.navigation.navigate({ routeName: 'MealDetail' });
-      }} />
-      <Button title="Go Back" onPress={() => {
-        props.navigation.goBack();
-      }} />
+      <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem} style={{ width: '100%' }} />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title
+  }
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 15
   }
 });
 
