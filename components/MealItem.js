ee004eac5b4634d3936aeb16a8d05001c2fa4ad7
Pawel Bak <pawelbak300589@gmail.com>
2021-03-07 14:42:22 +0000

    Add DefaultText component to set default fontstyle

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Platform, TouchableNativeFeedback, ImageBackground } from 'react-native';
 
+import DefaultText from './DefaultText';
+
 const MealItem = (props) => {
   let TouchableCmp = TouchableOpacity;
 
@@ -20,9 +22,9 @@ const MealItem = (props) => {
             </ImageBackground>
           </View>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <Text>{props.duration}m</Text>
-            <Text>{props.complexity.toUpperCase()}</Text>
-            <Text>{props.affordability.toUpperCase()}</Text>
+            <DefaultText>{props.duration}m</DefaultText>
+            <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+            <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
           </View>
         </View>
       </TouchableCmp>
