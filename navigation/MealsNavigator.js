eb8eb946ad206914b08a526cc9bdcc6da1ac9676
Pawel Bak <pawelbak300589@gmail.com>
2021-03-07 14:34:07 +0000

    Update fonts in the top and bottom navigation text

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -19,7 +19,14 @@ const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
   },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
+  },
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+  headerTitle: 'A Screen'
 };
 
 const MealsNavigator = createStackNavigator({
@@ -52,7 +59,8 @@ const tabScreenConfig = {
           color={tabInfo.tintColor}
         />);
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Meals</Text> : 'Meals'
     }
   },
   Favourites: {
@@ -61,7 +69,8 @@ const tabScreenConfig = {
       tabBarIcon: tabInfo => {
         return (<Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />);
       },
-      tabBarColor: Colors.accentColor
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Favourites</Text> : 'Favourites'
     }
   }
 };
@@ -72,12 +81,18 @@ const MealsFavTabNavigator =
       tabScreenConfig,
       {
         activeColor: 'white',
-        shifting: true
+        shifting: true,
+        barStyle: {
+          backgroundColor: Colors.primaryColor
+        }
       })
     : createBottomTabNavigator(
       tabScreenConfig,
       {
         tabBarOptions: {
+          labelStyle: {
+            fontFamily: 'open-sans-bold'
+          },
           activeTintColor: Colors.accentColor
         }
       }
