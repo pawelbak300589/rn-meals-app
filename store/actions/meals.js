3787c23e90e846fe0ee1914405f97b3fa13442f0
Pawel Bak <pawelbak300589@gmail.com>
2021-03-23 23:20:33 +0000

    Add set filters action and reducer case

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -1,5 +1,10 @@
 export const TOGGLE_FAVOURITES = 'TOGGLE_FAVOURITES';
+export const SET_FILTERS = 'SET_FILTERS';
 
 export const toggleFavourite = (id) => {
   return { type: TOGGLE_FAVOURITES, mealId: id };
+};
+
+export const setFilters = (filterSettings) => {
+  return { type: SET_FILTERS, filters: filterSettings };
 };
