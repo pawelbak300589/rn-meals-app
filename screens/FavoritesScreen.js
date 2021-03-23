60f038d0dd4a3a9aff715ac132e2369b397a9193
Pawel Bak <pawelbak300589@gmail.com>
2021-03-23 22:14:13 +0000

    Switch meals data imports to redux selectors

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import MealList from '../components/MealList';
 import HeaderButton from '../components/HeaderButton';
 
 const FavoritesScreen = (props) => {
-
-  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+  const favMeals = useSelector(state => state.meals.favouriteMeals);
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
